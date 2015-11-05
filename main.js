fa3e7734badb8d504d389db5b6488b3bae83a6af
Meredith Salguero <salguero.meredith@gmail.com>
2015-11-05 12:46:11 -0500

    set up HTML and CSS also defined variables in Jquery.

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('linked');
+$(document).ready(function(){
+	var $roundCount = $('#round');
+	var $redTile = $('#red');
+	var $blueTile = $('#blue');
+	var $yellowTile = $('#yellow');
+	var $greenTile = $('#green');
+	var $playButton = $('#play');
+
+
+
+});
