93d9603eae761951f809b19042e3d61c84e3352b
Meredith Salguero <salguero.meredith@gmail.com>
2015-11-07 10:18:16 -0500

    added notes to html and centered board as well as Moves

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	var $blueTile = $('#blue');
 	var $yellowTile = $('#yellow');
 	var $greenTile = $('#green');
-	var $playButton = $('#play');
+	var $playButton = $('#play');							//defining of variables
 	var $compTurn = $('#computerTurn');
 	var $playTurn = $('#playerTurn');
 	var $playWon = $('#playerWon');
@@ -18,41 +18,42 @@ $(document).ready(function(){
 
 
 	var getRandomColor=function(){		// this function gets one of 4 colors randomly and pushes them into randomColors array.
-		$round.text("Round: " + roundCount);
-		$playLost.css('visibility','hidden');
-		$compTurn.css('visibility','visible');
-		var rando=Math.floor(Math.random() * (4 - 1 + 1)) + 1; // from mdn for math.random() definition
+		$round.text("Round: " + roundCount); //this displays the round number the player is on
+		$playLost.css('visibility','hidden'); //this hides the player lost message
+		$compTurn.css('visibility','visible'); //this showes it is the computer's turn message
+		var rando=Math.floor(Math.random() * (4 - 1 + 1)) + 1; // from mdn for math.random() definition, gets random number between 1 & 4
 		if(rando===1){																
 			randomColors.push('red');
 			}else if(rando===2){
 			randomColors.push('blue');
-			}else if(rando===3){
+			}else if(rando===3){								//assigns on of the numbers to a specific color and pushes into array randomColors
 			randomColors.push('yellow');
 			}else if(rando===4){
 			randomColors.push('green');
 		}
-		randomColors.forEach(animateCompTile); //this is making it so when the computer gets the tiles it chooses and saves into an array will animate
-		setTimeout(function(){$compTurn.css('visibility','hidden')},randomColors.length*1000);	// this modeled after code I found on http://snipplr.com/view/19392/
-		setTimeout(function(){$playTurn.css('visibility','visible')},randomColors.length*1000);
-		$moves.text('Moves: '+randomColors.length);
-		numMoves = randomColors.length;
-		console.log(numMoves);
+		randomColors.forEach(animateCompTile); //this is making it so the randomColors array can be aniameted
+		setTimeout(function(){$compTurn.css('visibility','hidden')},randomColors.length*1000);	// this modeled after code I found on http://snipplr.com/view/19392/ , it allows the "computer's turn" message to disappear a second after the array has finished animating
+		setTimeout(function(){$playTurn.css('visibility','visible')},randomColors.length*1000); // same idea for this one but "Player's Turn" message is made visible
+		$moves.text('Moves: '+randomColors.length); //this shows how many moves the computer made
+		numMoves = randomColors.length; // this is defining a variable that will be used to show how many moves the player has left to go
 	};
+
+	$playButton.on('click',getRandomColor); //this event listener starts the game
 	
-	var getNumMoves = function(){
+	var getNumMoves = function(){		//this function shows the player how many moves they have left
 		numMoves = numMoves-1;
 		$moves.text('Moves: '+numMoves);
 	}
 	
-	$redTile.on('click', getNumMoves);
+	$redTile.on('click', getNumMoves);  //these events use the callback function getNumMoves to subtract from the number of moves each time one is clicked
 	$blueTile.on('click', getNumMoves);
 	$yellowTile.on('click', getNumMoves);
 	$greenTile.on('click', getNumMoves);
 
-	$playButton.on('click',getRandomColor);
+	
 
 
-	var animateCompTile = function(name,i){	// this modeled after code I found on snipplr.com
+	var animateCompTile = function(name,i){	// this modeled after code I found on snipplr.com, this function will be used to animated the randomColors array.  It ensures there is a delay between each item in the array
 		setTimeout(function(){
 			$('#'+name).addClass('pulse')
 				setTimeout(function(){
@@ -61,35 +62,35 @@ $(document).ready(function(){
 			},i*800)
 	};
 
-	var playedRed=function(){
+	var playedRed=function(){ 		//this records the palyer's choices into an array and adds animation on the div on which they selected
 		playerMoves.push('red');
 		$redTile.addClass('pulse');
 		setTimeout(function(){$redTile.removeClass('pulse')}, 500);
 	};
 	$redTile.on('click',playedRed);
 
-	var playedBlue=function(){
+	var playedBlue=function(){		//this records the palyer's choices into an array and adds animation on the div on which they selected
 		playerMoves.push('blue');
 		$blueTile.addClass('pulse');
 		setTimeout(function(){$blueTile.removeClass('pulse')}, 500);
 	};
 	$blueTile.on('click',playedBlue);
 
-	var playedYellow=function(){
+	var playedYellow=function(){		//this records the palyer's choices into an array and adds animation on the div on which they selected
 		playerMoves.push('yellow');
 		$yellowTile.addClass('pulse');
 		setTimeout(function(){$yellowTile.removeClass('pulse')}, 500);
 	};
 	$yellowTile.on('click',playedYellow);
 	
-	var playedGreen=function(){
+	var playedGreen=function(){		//this records the palyer's choices into an array and adds animation on the div on which they selected
 		playerMoves.push('green');
 		$greenTile.addClass('pulse');
 		setTimeout(function(){$greenTile.removeClass('pulse')}, 500);
 	};
 	$greenTile.on('click',playedGreen);
 
-var compareArrays = function(){
+var compareArrays = function(){ 		// this function is used to compare the player's choices with the computers and will notify the player if they have won or lost
 	if(event.which===13){
 		$playTurn.css('visibility','hidden');
 		roundCount+=1;
@@ -117,7 +118,7 @@ var compareArrays = function(){
 	}
 };
 
-$(document).keypress(compareArrays);
+$(document).keypress(compareArrays); // this event listener signifies the player has played their turn and is submitting their answer for review.  the compareArrays function is triggered by this listener event
 
 
 });
